2f646e02ce0a72e7fd134c4cde30e94767dec664
Roman Reprintsev <doberbeatz@gmail.com>
2018-02-18 18:17:50 +0300

    Init Files

---
+++ web/front.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
+
+function render_template($request) {
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    /** @var string $_route */
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
+$request = Request::createFromGlobals();
+$routes = include __DIR__ . '/../src/app.php';
+
+$context = new Routing\RequestContext();
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+$framework = new \Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
+
+$response->send();
